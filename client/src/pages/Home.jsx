e8d3ef4f40ef108c9325bda96126693b9e35aa28
Manpreet15kaur <manpreetkaur81008@gmail.com>
2025-07-12 22:34:29 +0530

    added moviecard and trailersSection

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,15 @@
 
 import React from 'react'
 import HeroSection from '../components/HeroSection'
+import FeaturedSection from '../components/FeaturedSection'
+import TrailorSection from '../components/TrailersSection'
 
 const home = () => {
   return (
     <>
 <HeroSection />
+<FeaturedSection/>
+<TrailorSection/>
 
     </>
   )

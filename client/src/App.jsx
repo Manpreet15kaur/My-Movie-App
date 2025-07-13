d51349bf20db569a5b708d72b20b01456b1b837d
Manpreet15kaur <manpreetkaur81008@gmail.com>
2025-07-13 20:40:27 +0530

    added footer and fovorites

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,33 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import {Route, Routes ,useLocation} from 'react-router-dom'
-import Home from './pages/Home'
-import Movie from './pages/Movie'
-import MovieDetails from './pages/MovieDetails'
-import SeatLayout from './pages/SeatLayout'
-import MyBookings from './pages/MyBookings'
-import Favourite from './pages/Favourite'
-import {Toaster} from  'react-hot-toast'
-import Footer from './components/Footer'
+import React from 'react';
+import Navbar from './components/Navbar';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import Home from './pages/Home';
+import Movies from './pages/Movies';
+import MovieDetails from './pages/MovieDetails';
+import SeatLayout from './pages/SeatLayout';
+import MyBookings from './pages/MyBookings';
+import Favourite from './pages/Favourite';
+import { Toaster } from 'react-hot-toast';
+import Footer from './components/Footer';
 
 const App = () => {
+  const isAdminRoute = useLocation().pathname.startsWith('/admin');
 
-  const isAdminRoute = useLocation().pathname.startsWith('/admin')
   return (
     <>
-    <Toaster/>
-    {!isAdminRoute && <Navbar/>}
+      <Toaster />
+      {!isAdminRoute && <Navbar />}
       <Routes>
-        <Route path ='/' element={<Home/>} />
-        <Route path ='/Movie' element={<Movie/>} />
-        <Route path ='/Movie/:id' element={<MovieDetails/>} />
-        <Route path ='/Movie/:id/:date' element={<SeatLayout/>} />
-         <Route path ='/MyBookings' element={<MyBookings/>} />
-          <Route path ='/favourite' element={<Favourite/>} />
-        
+        <Route path='/' element={<Home />} />
+        <Route path='/movies' element={<Movies />} />
+        <Route path='/movies/:id' element={<MovieDetails />} />
+        <Route path='/movies/:id/:date' element={<SeatLayout />} />
+        <Route path='/mybookings' element={<MyBookings />} />
+        <Route path='/favourite' element={<Favourite />} />
       </Routes>
-      {!isAdminRoute && <Footer/>}
+      {!isAdminRoute && <Footer />}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,14 +2,14 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection'
 import FeaturedSection from '../components/FeaturedSection'
-import TrailorSection from '../components/TrailersSection'
+import TrailersSection from '../components/TrailersSection'
 
 const home = () => {
   return (
     <>
 <HeroSection />
 <FeaturedSection/>
-<TrailorSection/>
+<TrailersSection/>
 
     </>
   )
